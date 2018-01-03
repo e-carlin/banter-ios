20bc7208bbd0f7716b2c9c0aef4596c7bbbaca90
Evan Carlin <evan@carlin.com>
2018-01-02 21:42:09 -0800

    Base user login with firebase working

---
+++ Banter/ResetPasswordViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ResetPasswordViewController.swift
+//  Banter
+//
+//  Created by e-carlin on 1/2/18.
+//  Copyright © 2018 Banter. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Forgot Password"
+        emailTextField.becomeFirstResponder()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
